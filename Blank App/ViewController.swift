e05da56d471ba1507a30e00a5e9a38fea55fcaf6
Bryan Clark <contact@bryanjclark.com>
2018-03-13 09:41:39 -0700

    Initial Commit

---
+++ Blank App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blank App
+//
+//  Created by Bryan Clark on 3/13/18.
+//  Copyright © 2018 Bryan Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
